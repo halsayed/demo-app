09ffeb08098d9d1f4594ce8dbf2834bfcda4656f
Husain Ebrahim <husain@alsayed.ws>
2022-07-27 22:46:36 +0300

    update

--- core/config.py
+++ core/config.py
@@ -8,7 +8,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-key'
     LOG_LEVEL = os.environ.get('LOG_LEVEL', logging.INFO)
 
-    VERSION = os.environ.get('VERSION', 'v1.0QR')
+    VERSION = os.environ.get('VERSION', 'v1.0')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///tmp.db')
 
